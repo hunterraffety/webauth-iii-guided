e0efa897a3e6459fcf84f2b58212628650182396
hsrsbd <hsraffety@gmail.com>
2019-07-24 10:39:27 -0700

    guided continued.

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,24 +1,33 @@
 const bcrypt = require('bcryptjs');
-
+const jwt = require('jsonwebtoken');
 const Users = require('../users/users-model.js');
+const secrets = require('../config/secrets');
 
 module.exports = (req, res, next) => {
-  const { username, password } = req.headers;
-
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({ message: 'Ran into an unexpected error' });
-      });
+  // get the token from Authorization header
+  const token = req.headers.authorization;
+  // verify the token
+  if (token) {
+    jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ message: 'Invalid token.' });
+      } else {
+        req.jwtToken = decodedToken;
+        next();
+      }
+    });
   } else {
-    res.status(400).json({ message: 'No credentials provided' });
+    res.status(400).json({ message: 'No token.' });
   }
 };
+
+/*
+//const options = {
+//  headers: {
+//  authorization: token
+//  }
+//}
+*/
+
+// axios.get(url, options);
+// axios.post(url, options);
